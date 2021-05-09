45fa8c373db455d77e2f7f722d2eafb4d7dfa016
trrfrr <nabooo007@gmail.com>
2021-05-09 22:00:33 +0100

    creating database + inserting data with fixture bundle

--- src/Kernel.php
+++ src/Kernel.php
@@ -9,10 +9,12 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
+
     use MicroKernelTrait;
 
     protected function configureContainer(ContainerConfigurator $container): void
     {
+
         $container->import('../config/{packages}/*.yaml');
         $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
 
